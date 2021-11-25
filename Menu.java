3eb0abd5362c753fc0f175e18356d5c20f4f2b28
Ilma Shaharin <49490075+ilma96@users.noreply.github.com>
2021-11-24 19:58:42 -0500

    Update Menu Class
    Menu is an interface and MenuObserver implements it.

--- Menu.java
+++ Menu.java
@@ -1,30 +1,7 @@
-// 1. [If we can figure this out!!] This also displays price(s)
-// --> [old price, updated price after coupon].
-// ***sellers can not use any coupon, so they have one price. ?
-// [Yet to be completed]
-// 2. There will two kinds of price --> Seller's price and Customer's price. ?
-// [Yet to be completed]
-//3. Menu class interacts with the Inventory class to know if a
-// product is in stock or not. [Yet to be completed]
-//4. Display the product names.
+//1. Menu class interacts with the Inventory class to know if a product is in stock or not. [Completed]
 
-public class Menu {
-
-    private float oldPrice;
-    private float priceAfterCoupon;
-
-    private float sellerPrice;
-    private float customerPrice;
-
-    private Menu menu = new Menu();
-    public void updateStock(){
-        System.out.println("Stock is being updated");
-    }
-
-    public void displayProducts(){
-        System.out.println("Display Product name");
-    }
-
-
-
+public interface Menu {
+     void attach(Inventory is); //for in-stock products
+     void detach(Inventory oos); //for out-of-stock products
+     void notifyUpdate(Message m);
 }
