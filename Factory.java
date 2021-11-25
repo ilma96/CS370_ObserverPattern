fb3fa0acbe985bc483985d36af15ee8f5ab4640d
Ilma Shaharin <49490075+ilma96@users.noreply.github.com>
2021-11-24 19:46:47 -0500

    Renamed Factory into FactoryProductStatusOne
    Each FactoryProductStatus contains an update method that updates a message for a particular item.

--- Factory.java
+++ Factory.java
@@ -1,27 +1,11 @@
-public class Factory extends Inventory {
-    // 1. Factory class is extending from Inventory class because it is an observer.
-    //2.Does Product processing, shipping, Quality checking etc.
-    //3.Factory interacts with the Menu class to add products to process, quality check, and ship.
+public class FactoryProductStatusOne implements Inventory {
+    // 1. Factory class is implementing Inventory Interface because it is the Observer.
+    //2. Overrides update() from Inventory Interface to print out a message from Message Class.
+  
 
-    public void productProcessing(){
-        System.out.println("Product processing method");
+    @Override
+    public void update(Message m) {
+        System.out.println("The item, 'Kitchen Knife Set' : " + m.getMessageContent());
     }
-
-    public void shipping(){
-        System.out.println("Shipping method");
-    }
-
-    public void updateStock(){
-        System.out.println("Quality check method");
-    }
-
-    private Factory factory = new Factory();
-
-    Menu m1 = new Menu(); // m1 is used to access Menu; methods
-
-    public Factory(){ // constructor
-        super(); // this method is used to call the Inventory class variable and methods
-    }
-
-
 }
+
